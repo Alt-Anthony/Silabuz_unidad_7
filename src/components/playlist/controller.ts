6478b34d7b5af2b4580d2e4ae4ef6aa39ad97e62
Anthony Moises Condori Choccata <113246456+Alt-Anthony@users.noreply.github.com>
2023-01-23 17:29:06 -0500

    agregando addsong to Playlist

--- src/components/playlist/controller.ts
+++ src/components/playlist/controller.ts
@@ -45,5 +45,34 @@ export const postPlaylist = async(req:Request,res:Response):Promise<void>=>{
     }
 };
 
+export const addSong =async (req:Request,res:Response):Promise<void> => {
+    try{
+     const data = req.body;
+ 
+     const playlist = await prisma.playlist.update({
+         where: {
+             id: data.id.playlist,
+         },
+         include:{
+             songs: true,
+         },
+         data:{
+             songs: { connect:{id: data.id_song}},
+         },
+     });
+     res.json({
+         message:"ADDED",
+         info: playlist,
+     });
+    }
+    catch(error){
+     res.status(500).json({
+         message:error
+     });
+     console.log(error); 
+    }
+ }
+ 
+
 
 //aqui van los ocntroladores
--- src/components/playlist/index.ts
+++ src/components/playlist/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getPlaylists, postPlaylist } from "./controller";
+import { getPlaylists, postPlaylist, addSong } from "./controller";
 
 const rutaPlaylists: Router = Router();
 
 rutaPlaylists.get("/", getPlaylists);
 rutaPlaylists.post("/", postPlaylist);
+rutaPlaylists.put("/add-song", addSong);
+
 export default rutaPlaylists;
